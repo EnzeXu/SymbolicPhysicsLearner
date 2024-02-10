8930448c5200d593f6de87fc82c34518b5662f6d
EnzeXu <xue20@wfu.edu>
2024-02-10 01:42:22 -0500

    init

---
+++ jobs/job_spl_20240210_run_2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for noise_ratio in "0.002" # "0.000" "0.002" "0.004" "0.006" "0.008" "0.010"
+do
+    for seed in "0" "1" "2"
+    do
+        for task in "Lotka_Volterra" "SIR" "Lorenz"
+        do
+            for eq_id in "1" "2"
+            do
+                for env_id in "0" "1" "2" "3" "4"
+                do
+                    echo "python spl_train.py --task ${task} --num_env 5 --num_run 1 --task_ode_num ${eq_id} --env_id ${env_id} --seed ${seed} --noise_ratio ${noise_ratio}"
+                    python spl_train.py --task ${task} --num_env 5 --num_run 1 --task_ode_num ${eq_id} --env_id ${env_id} --seed ${seed} --noise_ratio ${noise_ratio}
+                done
+            done
+        done
+    done
+done
+
+
+
