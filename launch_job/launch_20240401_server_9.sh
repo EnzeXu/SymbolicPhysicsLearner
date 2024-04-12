c2cfb68a27604cc20f9229e9a14bbb21a587b83e
EnzeXu <xue20@wfu.edu>
2024-04-12 14:07:48 -0400

    update spl

--- launch_job/launch_20240401_server_9.sh
+++ launch_job/launch_20240401_server_9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/exu03/workspace/SymbolicPhysicsLearner
-bash launch_job/process_20240401_pp_default_2_9.sh
-bash launch_job/process_20240401_sir_default_2_9.sh
-bash launch_job/process_20240401_lorenz_default_2_9.sh
+bash launch_job/process_20240401_pp_default_3_9.sh
+bash launch_job/process_20240401_sir_default_3_9.sh
+bash launch_job/process_20240401_lorenz_default_3_9.sh
